0d5f41e363f971c567815bfd8cde8ed1b1e7df48
Geocarlos Alves <geowildcat@gmail.com>
2019-09-15 18:46:08 -0400

    Update presentation video

--- src/app/homepage/about/about.component.ts
+++ src/app/homepage/about/about.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
+import { AppLanguage } from 'src/app/app.language';
 
 @Component({
   selector: 'app-about',
@@ -7,7 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutComponent implements OnInit {
 
-  constructor() { }
+  language = AppLanguage.getLanguage();
+
+  constructor(public sanitizer: DomSanitizer) { }
+
+  get videoId(): string {
+    console.log(this.language);
+    return this.language === 'en' ? '50280kjWeLQ' : 'V9y0ZbR3Yxc';
+  }
 
   ngOnInit() {
     window.scrollTo(0, 0);
